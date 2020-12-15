d77aab28e5047d9170422c648ab491e9d9b6407f
Bot hub offical <76042379+BotHub-1@users.noreply.github.com>
2020-12-15 16:23:03 +0530

    Update __main__.py
    Ehh

--- bot/__main__.py
+++ bot/__main__.py
@@ -174,7 +174,7 @@ def send_start(bot, update):
 
     keyboard = [[InlineKeyboardButton(text="💁Help",callback_data="help_back"),InlineKeyboardButton(text="😎Master😎",url="https://t.me/Bot_Hub1")]]
     keyboard += [[InlineKeyboardButton(text="😪Connect Group😪", callback_data="main_connect"),InlineKeyboardButton(text="🎗️Add Me🎗️",url="t.me/{}?startgroup=true".format(bot.username))]]
-    Keyboard += [[InlinekeyboardButton(text="🎉Support🎉",url="t.me/Bot_Hub1")]]
+    keyboard += [[InlinekeyboardButton(text="🎉Support🎉",url="t.me/Bot_Hub1")]]
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
